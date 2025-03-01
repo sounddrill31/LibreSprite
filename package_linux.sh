a6743bfa61041f0cd94e30c3f5e3e8f9939eb570
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-01 01:42:30 -0800

    Fix path to libresprite

--- package_linux.sh
+++ package_linux.sh
@@ -20,8 +20,9 @@ cd LibreSprite
 wget "https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin" -O ./lib4bin
 chmod +x ./lib4bin
 xvfb-run -a -- ./lib4bin -p -v -e -r -k -w \
-  /usr/bin/libresprite \
-  /usr/bin/data \
+  ../libresprite \
+  ../data \
+  ../*.so* \
   /usr/lib/libpthread.so* \
   /usr/lib/librt.so* \
   /usr/lib/libstdc++.so* 
